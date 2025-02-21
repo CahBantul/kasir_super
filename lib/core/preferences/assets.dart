f66f0851cdfbff245d4eaea89afee5101f6d16dc
Fardan Nozami Ajitama <fardan.nozami@gmail.com>
2025-02-21 16:13:48 +0700

    adding main constanta

---
+++ lib/core/preferences/assets.dart
@@ -0,0 +1,12 @@
+class MainAssets {
+  MainAssets._();
+
+  static const path = 'assets/images';
+
+  static const logo = '$path/logo.png';
+  static const error = '$path/error.png';
+  static const empty = '$path/empty.png';
+  static const printer = '$path/printer.png';
+  static const success1 = '$path/success1.png';
+  static const success2 = '$path/success2.png';
+}
